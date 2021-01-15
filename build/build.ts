7a0c85ae224de8fe3988e161c3173c738325deb0
Daniel Wolf <dwolf@dannad.de>
2021-01-15 18:21:59 +0100

    Change output format from source code to JSON files

--- build/build.ts
+++ build/build.ts
@@ -1,6 +1,6 @@
 import { generateStats } from "./generate-stats";
 import { createMultiLangDictionary, MultiLangDictionary } from "./multi-lang-dictionary";
-import { generateSources } from './generate-sources';
+import { generateDataFiles } from './generate-data-files';
 import { pronunciationSourceWiktionaryEn } from "./pronunciation-sources.ts/pronunciation-source-wiktionary-en";
 import { pronunciationSourceWiktionaryDe } from "./pronunciation-sources.ts/pronunciation-source-wiktionary-de";
 import { PronunciationResult } from "./pronunciation-sources.ts/pronunciation-source";
@@ -8,7 +8,7 @@ import { runAsyncMain } from "./utils/run-async-main";
 import { wiktionaryEditionToString } from "./wiktionary/wiktionary-edition";
 
 function omitSparseLanguages(multiLangDictionary: MultiLangDictionary): MultiLangDictionary {
-  const minWordCount = 500;
+  const minWordCount = 10000;
   return new Map([...multiLangDictionary].filter(([_, words]) => words.size >= minWordCount));
 }
 
@@ -30,10 +30,11 @@ async function main() {
     console.log('Generating statistics.');
     generateStats(pronunciationResults);
 
+    console.log('Assembling multi-language dictionary.');
     const multiLangDictionary = omitSparseLanguages(createMultiLangDictionary(pronunciationResults));
 
-    console.log('Generating source files.');
-    generateSources(multiLangDictionary);
+    console.log('Generating data files.');
+    generateDataFiles(multiLangDictionary);
   } catch (error) {
     console.error(error, error.stack);
     process.exit(1);
