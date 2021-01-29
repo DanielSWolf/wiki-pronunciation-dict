443b26b0432b917b462f03c762ec8a0e636f1a7a
Daniel Wolf <dwolf@dannad.de>
2021-01-29 15:25:06 +0100

    Make download code more generic

--- build/utils/download-bzip2-content.ts
+++ build/utils/download-bzip2-content.ts
@@ -1,27 +1,44 @@
 import download from 'download';
 import streamProgressbar from 'stream-progressbar';
-import { createWriteStream, ensureDir, move } from 'fs-extra';
-import { dirname } from 'path';
+import { createWriteStream, ensureDir, existsSync, move } from 'fs-extra';
+import { dirname, basename } from 'path';
 import unbzip2Stream from 'unbzip2-stream';
 
+interface FileDownloadOptions {
+  description?: string;
+  decompressBzip2?: boolean;
+  skipIfExists?: boolean;
+}
+
 /**
- * Downloads a bzip2-compressed file, performs a streaming decompression and stores it with the
- * specified target path. Shows a progress bar during download.
+ * Performs a streaming file download, optionally combined with streaming bzip2 decompression, and
+ * stores the result with the specified target path. Shows a progress bar during download.
  *
  * The target file is created in one atomic operation, so that if the process is terminated, the
  * target file is either complete or missing.
  */
-export async function downloadBzip2Content(
+export async function downloadFile(
   url: string,
   targetPath: string,
+  options?: FileDownloadOptions,
 ): Promise<void> {
   await ensureDir(dirname(targetPath));
 
+  if (options?.skipIfExists && existsSync(targetPath)) {
+    return;
+  }
+
+  const description = options?.description ?? basename(targetPath);
+  console.log(`Downloading ${description}.`);
+
   const tempPath = `${targetPath}~`;
-  const stream = download(url)
-    .pipe(streamProgressbar(':bar :percent downloaded (:etas remaining)'))
-    .pipe(unbzip2Stream())
-    .pipe(createWriteStream(tempPath));
+  let stream: NodeJS.ReadWriteStream = download(url).pipe(
+    streamProgressbar(':bar :percent downloaded (:etas remaining)'),
+  );
+  if (options?.decompressBzip2) {
+    stream = stream.pipe(unbzip2Stream());
+  }
+  stream.pipe(createWriteStream(tempPath));
   await new Promise((resolve, reject) =>
     stream.on('close', resolve).on('error', reject),
   );
