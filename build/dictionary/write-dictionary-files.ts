f421b44f584d138b110cef953252de3e2f77a74c
Daniel Wolf <dwolf@dannad.de>
2021-05-22 14:31:44 +0200

    Include grapheme and phoneme distributions in exported metadata

--- build/dictionary/write-dictionary-files.ts
+++ build/dictionary/write-dictionary-files.ts
@@ -5,7 +5,10 @@ import { LanguageLookup } from '../languages/language-lookup';
 import { englishCollator, getCollator } from '../utils/collation';
 import { toCompactJson } from '../utils/to-compact-json';
 import { Dictionary, DictionaryData } from './create-dictionary';
-import { getName } from '@cospired/i18n-iso-languages';
+import { DefaultMap } from '../utils/default-map';
+import { sortMap } from '../utils/sort-map';
+import inspect from 'object-inspect';
+import { getLanguageName } from '../language';
 
 export function writeDictionaryFiles(dictionaries: Dictionary[]) {
   emptyDirSync(dictionariesDir);
@@ -18,6 +21,7 @@ export function writeDictionaryFiles(dictionaries: Dictionary[]) {
       writeMetadataFile(
         `${dictionary.language}-metadata.json`,
         dictionary.languageLookup,
+        dictionary.data,
       );
     }
   }
@@ -44,10 +48,11 @@ function writeDataFile(fileName: string, data: DictionaryData) {
 function writeMetadataFile(
   fileName: string,
   languageLookup: LanguageLookup<any, any>,
+  data: DictionaryData,
 ) {
   const { language } = languageLookup;
 
-  const languageName = getName(language, 'en');
+  const languageName = getLanguageName(language);
 
   // Order graphemes using language-specific sorting rules
   const graphemes = [...languageLookup.graphemes].sort(
@@ -57,9 +62,79 @@ function writeMetadataFile(
   // Order phonemes using English sorting rules
   const phonemes = [...languageLookup.phonemes].sort(englishCollator.compare);
 
+  const graphemeDistribution = Object.fromEntries(
+    getGraphemeDistribution(languageLookup, data),
+  );
+  const phonemeDistribution = Object.fromEntries(
+    getPhonemeDistribution(languageLookup, data),
+  );
+
   const path = joinPaths(dictionariesDir, fileName);
   writeFileSync(
     path,
-    toCompactJson({ language, languageName, graphemes, phonemes }),
+    toCompactJson({
+      language,
+      languageName,
+      graphemes,
+      phonemes,
+      graphemeDistribution,
+      phonemeDistribution,
+    }),
   );
+}
+
+function getGraphemeDistribution(
+  languageLookup: LanguageLookup<any, any>,
+  data: DictionaryData,
+): ReadonlyMap<string, number> {
+  const graphemeSet = new Set(languageLookup.graphemes);
+
+  const result = new DefaultMap<string, number>(() => 0);
+  for (const word of data.keys()) {
+    for (const grapheme of [...word]) {
+      if (!graphemeSet.has(grapheme)) {
+        const context = { language: languageLookup.language, word };
+        throw new Error(
+          `Invalid grapheme ${inspect(grapheme)}. ${inspect(context)}`,
+        );
+      }
+
+      result.set(grapheme, result.getOrCreate(grapheme) + 1);
+    }
+  }
+
+  // Order by descending frequency
+  sortMap(result, (a, b) => b[1] - a[1]);
+  return result;
+}
+
+function getPhonemeDistribution(
+  languageLookup: LanguageLookup<any, any>,
+  data: DictionaryData,
+): ReadonlyMap<string, number> {
+  const phonemeSet = new Set(languageLookup.phonemes);
+
+  const result = new DefaultMap<string, number>(() => 0);
+  for (const [word, pronunciations] of data.entries()) {
+    for (const pronunciation of pronunciations) {
+      for (const phoneme of pronunciation.split(' ')) {
+        if (!phonemeSet.has(phoneme)) {
+          const context = {
+            language: languageLookup.language,
+            word,
+            pronunciation,
+          };
+          throw new Error(
+            `Invalid phoneme ${inspect(phoneme)}. ${inspect(context)}`,
+          );
+        }
+
+        result.set(phoneme, result.getOrCreate(phoneme) + 1);
+      }
+    }
+  }
+
+  // Order by descending frequency
+  sortMap(result, (a, b) => b[1] - a[1]);
+  return result;
 }
--- build/language.ts
+++ build/language.ts
@@ -1,2 +1,8 @@
+import { getName } from '@cospired/i18n-iso-languages';
+
 /** An ISO 639-1 language code */
 export type Language = string;
+
+export function getLanguageName(language: Language) {
+  return getName(language, 'en') ?? `<${language}>`;
+}
